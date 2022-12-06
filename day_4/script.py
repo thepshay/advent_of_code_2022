c881a71efe57a0e085ce5fdbfd20b4c88e264f90
thepshay <perry.xie1@gmail.com>
2022-12-05 19:04:11 -0800

    fix indentation

--- day_4/script.py
+++ day_4/script.py
@@ -1,18 +1,18 @@
 with open('input.txt') as f:
   lines = f.readlines()
 
-  total = 0
+total = 0
 
-  for line in lines:
-    line = line.strip()
-    [worker1, worker2] = line.split(',')
+for line in lines:
+  line = line.strip()
+  [worker1, worker2] = line.split(',')
 
-    worker1_range = worker1.split('-')
-    worker2_range = worker2.split('-')
+  worker1_range = worker1.split('-')
+  worker2_range = worker2.split('-')
 
-    if (int(worker1_range[0]) <= int(worker2_range[0]) and int(worker1_range[1]) >= int(worker2_range[1])):
-      total += 1
-    elif (int(worker1_range[0]) >= int(worker2_range[0]) and int(worker1_range[1]) <= int(worker2_range[1])):
-      total += 1
+  if (int(worker1_range[0]) <= int(worker2_range[0]) and int(worker1_range[1]) >= int(worker2_range[1])):
+    total += 1
+  elif (int(worker1_range[0]) >= int(worker2_range[0]) and int(worker1_range[1]) <= int(worker2_range[1])):
+    total += 1
 
 print(total)
